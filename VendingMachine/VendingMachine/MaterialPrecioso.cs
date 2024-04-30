f807f8a149e61a77f26abf7c1147d8228a031bb5
@marichu_kt <145447256+marichu-kt@users.noreply.github.com>
2024-04-30 18:52:54 +0200

    Add files via upload

---
+++ VendingMachine/VendingMachine/MaterialPrecioso.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    internal class MaterialPrecioso : Producto
+    {
+        public string TipoMaterial { get; set; }
+        public double Peso { get; set; }
+
+        public override void MostrarInformacion()
+        {
+            base.MostrarInformacion();
+            Console.WriteLine($"Tipo de material: {TipoMaterial}");
+            Console.WriteLine($"Peso: {Peso} gramos");
+        }
+    }
+}
---
+++ VendingMachine/VendingMachine/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    internal class Producto
+    {
+        public string Nombre { get; set; }
+        public int Unidades { get; set; }
+        public double PrecioUnitario { get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Unidades disponibles: {Unidades}");
+            Console.WriteLine($"Precio unitario: {PrecioUnitario:N2}$");
+            Console.WriteLine($"Descripción: {Descripcion}");
+        }
+    }
+}
