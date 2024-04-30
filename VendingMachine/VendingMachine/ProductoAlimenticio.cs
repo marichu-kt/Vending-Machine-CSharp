6b6c58d8501f62f6ce8929de136dec64a516ceae
@marichu_kt <145447256+marichu-kt@users.noreply.github.com>
2024-04-30 18:56:00 +0200

    Add files via upload

---
+++ VendingMachine/VendingMachine/ProductoAlimenticio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    internal class ProductoAlimenticio : Producto
+    {
+        public string InformacionNutricional { get; set; }
+
+
+        public override void MostrarInformacion()
+        {
+            base.MostrarInformacion();
+            Console.WriteLine($"Información nutricional: {InformacionNutricional}");
+        }
+    }
+}
---
+++ VendingMachine/VendingMachine/ProductoElectronico.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    internal class ProductoElectronico : Producto
+    {
+        public string Materiales { get; set; }
+        public bool TieneBateria { get; set; }
+        public bool Precargado { get; set; }
+
+        public override void MostrarInformacion()
+        {
+            base.MostrarInformacion();
+            Console.WriteLine($"Materiales: {Materiales}");
+            Console.WriteLine($"Tiene batería: {(TieneBateria ? "Sí" : "No")}");
+            Console.WriteLine($"Precargado: {(Precargado ? "Sí" : "No")}");
+        }
+    }
+}
